7f77f21027ab43b2bab2076cd633323414ba4324
kaushalnagrale <kaushalnagrale@gmail.com>
2026-01-30 01:12:47 +0530

    Update requirements.py

--- requirements.py
+++ requirements.py
@@ -1,16 +1,11 @@
 streamlit
-pyhton-dotenv
+python-dotenv
 groq
 networkx
 matplotlib
 spacy
 pyvis
-en_core_web_sm
-"""python -m spacy download en_core_web_sm
-pip install matplotlib spacy pyvis
-pip install knowledge_extractor
-pip install groq
-pip install graph_builder
-pip install neo4j
-pip install py2neo
-"""
+neo4j
+py2neo
+knowledge-extractor
+graph-builder
